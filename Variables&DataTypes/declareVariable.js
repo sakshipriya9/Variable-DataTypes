e3215d69fdf6ba90d3bebaa6c2bd3c18682b937a
Sakshi Priya <priyasakshi8989@gmail.com>
2025-04-22 17:14:12 +0530

    Declare the variables

---
+++ Variables&DataTypes/declareVariable.js
@@ -0,0 +1,21 @@
+//Declare a variable in JS by using var, let and const
+
+var name = "Sakshi";
+let age = "22";
+const course_completed = "Full Stack Developer"; //Value will not be change after assigning with const keyword, i.e- line no-16
+
+//print all of them 
+console.log(name);
+console.log(age);
+console.log(course_completed);
+
+//Try to reassigning it 
+
+name = "Priya";
+age = "24";
+// course_completed = "Data Analyst";
+
+//print all of them again
+console.log("After reassign the value:",name);
+console.log("After reassign the value:",age);
+// console.log(course_completed);
